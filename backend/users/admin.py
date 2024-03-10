38d6ee2837c4feedadae0fa4c03774979e68fa7f
blazej <bbielski@lyck.pl>
2024-03-10 19:22:32 +0100

    change dockerfile, remove db file, change docker-compose, change user registration, add psycopg, add migrations for users

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from users import models
 
-admin.site.register("users", UserAdmin)
+
+admin.site.register(models.CustomUser)
