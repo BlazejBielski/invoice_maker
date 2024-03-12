0f8c978f5510414c0572d9dc7c84b33cc58d9b52
blazej <bbielski@lyck.pl>
2024-03-12 15:06:01 +0100

    change dockerfile`s and custom admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
 from users import models
 
 
-admin.site.register(models.CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = get_user_model()
+
+
+admin.site.register(models.CustomUser, CustomUserAdmin)
