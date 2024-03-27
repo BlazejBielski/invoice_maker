8ab1117fbfbea3103299f3de321b8cf1be275505
blazej <bbielski@lyck.pl>
2024-03-27 19:40:39 +0100

    change timestampmodel, add invoices and products models

--- backend/invoices/models.py
+++ backend/invoices/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from utils.timestamp import TimeStampModel
 
-# Create your models here.
+
+VAT_RATE_CHOICES = [
+    (23, "A stawka podstawowa podatku 23%"),
+    (8, "B stawka obniżona podatku 8%"),
+    (5, "C stawka obniżona podatku 5%"),
+    (0, "D stawka obniżona podatku 0%"),
+    (0, "E zwolnienie z podatku"),
+]
+
+
+class Products(TimeStampModel):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    vat_rate = models.IntegerField(choices=VAT_RATE_CHOICES, default=VAT_RATE_CHOICES[0])
+    comments = models.CharField(max_length=255)
+    pkd = models.CharField(max_length=7)
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Contractors(TimeStampModel):
+    pass
+
+
+class Invoices(TimeStampModel):
+    number = models.CharField()
+    products = models.ForeignKey(Products, on_delete=models.DO_NOTHING)
+    contractors = models.ForeignKey(Contractors, on_delete=models.DO_NOTHING)
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=4)
+
+    def __str__(self):
+        return self.owner
--- backend/utils/timestamp.py
+++ backend/utils/timestamp.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Timestamp(models.Model):
+class TimeStampModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
