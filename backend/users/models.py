6623d7bf1f1aa98ab638b307ca63a5af5f4b67db
blazej <bbielski@lyck.pl>
2024-05-05 07:40:34 +0200

    custom user manager, add user

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,33 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class CustomUserManagaer(BaseUserManager["CustomUser"]):
+
+    """Custom manager for CustomUser"""
+
+    use_in_migrations = True
+
+    def create_user(self, email, password, username="", **kwargs):
+        """Create a user with the given email and password.
+
+        :param str email: Email address of the user
+        :param str password: <PASSWORD>
+        :param str username: Username
+        :return users.models.CustomUser: User object
+        :raises ValueError: If email not set
+        """
+
+        if not email:
+            raise ValueError("Email address is required")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **kwargs)
+        user.is_active = False
+        user.password = make_password(password)
+        user.save()
 
 
 class CustomUser(AbstractUser):
